450172e2df4076664d48630db36a4180866dd647
teo <asselineteo@gmail.com>
2023-11-27 16:37:42 +0100

    Refactoring Hole3
    Co-authored-by: Yael Alfred <AlfredYael01@users.noreply.github.com>
    Co-authored-by: aniszi <aniszi@users.noreply.github.com>

--- src/12_RefactoringGolf/hole1/README.md
+++ src/12_RefactoringGolf/hole1/README.md
@@ -1,3 +1,7 @@
+# Participant
+
+Téo Asseline / Yael Alfred / Anis Zizi
+
 # Hole 3 to Hole 4
 
 Change the code in hole 3 to be identical to the code on hole 4, both implenentation and tests can change.
--- src/12_RefactoringGolf/hole1/kata.ts
+++ src/12_RefactoringGolf/hole1/kata.ts
@@ -1,7 +1,13 @@
 
-const leBonNom = ' ';
+const blankSpace = ' ';
+const player_o = 'O';
+const origin = 0 ;
+const INVALID_FIRST_PLAYER_MESSAGE = 'Invalid first player';
+const INVALID_NEXT_PLAYER_MESSAGE = 'Invalid next player';
+const INVALID_POSITION_MESSAGE = 'Invalid position';
+
 export class Game {
-  private _lastSymbol = leBonNom;
+  private _lastSymbol = blankSpace;
   private _board: Board = new Board();
 
   public Play(symbol: string, x: number, y: number): void {
@@ -14,22 +20,22 @@ export class Game {
   }
 
   private validateFirstMove(player: string) {
-    if (this._lastSymbol == ' ') {
-      if (player == 'O') {
-        throw new Error('Invalid first player');
+    if (this._lastSymbol == blankSpace) {
+      if (player == player_o) {
+        throw new Error(INVALID_FIRST_PLAYER_MESSAGE);
       }
     }
   }
 
   private validatePlayer(player: string) {
     if (player == this._lastSymbol) {
-      throw new Error('Invalid next player');
+      throw new Error(INVALID_NEXT_PLAYER_MESSAGE);
     }
   }
 
   private validatePositionIsEmpty(x: number, y: number) {
-    if (this._board.TileAt(x, y).Symbol != ' ') {
-      throw new Error('Invalid position');
+    if (this._board.TileAt(x, y).Symbol != blankSpace) {
+      throw new Error(INVALID_POSITION_MESSAGE);
     }
   }
 
@@ -43,15 +49,15 @@ export class Game {
 
   public Winner(): string {
     if (this.isFirstRowFull() && this.isFirstRowFullWithSameSymbol()) {
-      return this._board.TileAt(0, 0)!.Symbol;
+      return this._board.TileAt(origin, origin)!.Symbol;
     }
 
     if (this.isSecondRowFull() && this.isSecondRowFullWithSameSymbol()) {
-      return this._board.TileAt(1, 0)!.Symbol;
+      return this._board.TileAt(1, origin)!.Symbol;
     }
 
     if (this.isThirdRowFull() && this.isThirdRowFullWithSameSymbol()) {
-      return this._board.TileAt(2, 0)!.Symbol;
+      return this._board.TileAt(2, origin)!.Symbol;
     }
 
     return ' ';
@@ -59,45 +65,45 @@ export class Game {
 
   private isFirstRowFull() {
     return (
-      this._board.TileAt(0, 0)!.Symbol != ' ' &&
-      this._board.TileAt(0, 1)!.Symbol != ' ' &&
-      this._board.TileAt(0, 2)!.Symbol != ' '
+      this._board.TileAt(origin, origin)!.Symbol != blankSpace &&
+      this._board.TileAt(origin, 1)!.Symbol != blankSpace &&
+      this._board.TileAt(origin, 2)!.Symbol != blankSpace
     );
   }
 
   private isFirstRowFullWithSameSymbol() {
     return (
-      this._board.TileAt(0, 0)!.Symbol == this._board.TileAt(0, 1)!.Symbol &&
-      this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol
+      this._board.TileAt(origin, origin)!.Symbol == this._board.TileAt(origin, 1)!.Symbol &&
+      this._board.TileAt(origin, 2)!.Symbol == this._board.TileAt(origin, 1)!.Symbol
     );
   }
 
   private isSecondRowFull() {
     return (
-      this._board.TileAt(1, 0)!.Symbol != ' ' &&
-      this._board.TileAt(1, 1)!.Symbol != ' ' &&
-      this._board.TileAt(1, 2)!.Symbol != ' '
+      this._board.TileAt(1, origin)!.Symbol != blankSpace &&
+      this._board.TileAt(1, 1)!.Symbol != blankSpace &&
+      this._board.TileAt(1, 2)!.Symbol != blankSpace
     );
   }
 
   private isSecondRowFullWithSameSymbol() {
     return (
-      this._board.TileAt(1, 0)!.Symbol == this._board.TileAt(1, 1)!.Symbol &&
+      this._board.TileAt(1, origin)!.Symbol == this._board.TileAt(1, 1)!.Symbol &&
       this._board.TileAt(1, 2)!.Symbol == this._board.TileAt(1, 1)!.Symbol
     );
   }
 
   private isThirdRowFull() {
     return (
-      this._board.TileAt(2, 0)!.Symbol != ' ' &&
-      this._board.TileAt(2, 1)!.Symbol != ' ' &&
-      this._board.TileAt(2, 2)!.Symbol != ' '
+      this._board.TileAt(2, origin)!.Symbol != blankSpace &&
+      this._board.TileAt(2, 1)!.Symbol != blankSpace &&
+      this._board.TileAt(2, 2)!.Symbol != blankSpace
     );
   }
 
   private isThirdRowFullWithSameSymbol() {
     return (
-      this._board.TileAt(2, 0)!.Symbol == this._board.TileAt(2, 1)!.Symbol &&
+      this._board.TileAt(2, origin)!.Symbol == this._board.TileAt(2, 1)!.Symbol &&
       this._board.TileAt(2, 2)!.Symbol == this._board.TileAt(2, 1)!.Symbol
     );
   }
@@ -113,9 +119,9 @@ class Board {
   private _plays: Tile[] = [];
 
   constructor() {
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        const tile: Tile = { X: i, Y: j, Symbol: ' ' };
+    for (let i = origin; i < 3; i++) {
+      for (let j = origin; j < 3; j++) {
+        const tile: Tile = { X: i, Y: j, Symbol: blankSpace };
         this._plays.push(tile);
       }
     }
